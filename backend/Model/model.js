ba435d1e2faa043af2a9f064333f0f99ca91d6bd
ssantacruzpe <stephanysantacruz@protonmail.com>
2023-11-06 16:05:46 +0100

    task schema ready

---
+++ backend/Model/model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    taskName:{
+        type: String,
+        required: true
+    },
+
+    taskDeadline: {
+        type: Date,
+        required: true
+    },
+
+    taskStatus: {
+        type: String,
+        required:true,
+        enum: ["To Do", "In Progress", "Done"]
+    },
+
+    taskOwner:{
+        type: String,
+        required: true
+    },
+})
+
+const taskEntries = mongoose.model("taskEntries", taskSchema);
+
+module.export = taskEntries;
