b120471b51f2cdd1ef02283cb99a68531f8f58cc
ssantacruzpe <stephanysantacruz@protonmail.com>
2023-11-07 14:24:19 +0100

    Started work in the Welcome Page

--- client/src/Components/WelcomePage.js
+++ client/src/Components/WelcomePage.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./WelcomePage.css"; 
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 function WelcomePage(){
+
     return(
         <div>
             <h1>Welcome</h1>
