a7d263d271c19d4af3db1a59748ec89adc782223
ssantacruzpe <stephanysantacruz@protonmail.com>
2023-11-20 00:19:03 +0100

    update box not rendering current deadline, need to fix

--- client/src/Components/TaskBoard.js
+++ client/src/Components/TaskBoard.js
@@ -1,61 +1,151 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./TaskBoard.css"; 
+import "./TaskBoard.css";
 
 function TaskBoard() {
+    const [tasks, setTasks] = useState([]);
+    const [selectedTask, setSelectedTask] = useState(null);
+    const [showMenu, setShowMenu] = useState(false);
+    const [showUpdatePopup, setShowUpdatePopup] = useState(false);
+    const [showDeletePopup, setShowDeletePopup] = useState(false);
+    const [updatedTaskName, setUpdatedTaskName] = useState("");
+    const [updatedTaskDeadline, setUpdatedTaskDeadline] = useState("");
+    const [updatedTaskStatus, setUpdatedTaskStatus] = useState("");
+    const [updatedTaskOwner, setUpdatedTaskOwner] = useState("");
 
-  const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        fetchTasks();
+    }, []);
 
-  useEffect(() => {
-      const fetchTasks = async () => {
-          try {
-              const response = await axios.get("http://localhost:3000/hh/tasks");
-              setTasks(response.data.tasks);
-          } catch (error) {
-              console.error("Error fetching tasks:", error);
-          }
-      };
-      fetchTasks();
-  }, []);
+    const fetchTasks = async () => {
+        try {
+            const response = await axios.get("http://localhost:3000/hh/tasks");
+            setTasks(response.data.tasks);
+        } catch (error) {
+            console.error("Error fetching tasks:", error);
+        }
+    };
 
-  const filterTasksByStatus = (status) => {
-    return tasks.filter(task => task.taskStatus === status);
+    const handleMenuClick = (task) => {
+        setSelectedTask(task);
+        setShowMenu(!showMenu);
+    };
+
+    const handleUpdateClick = () => {
+      if (selectedTask) {
+          setUpdatedTaskName(selectedTask.taskName);
+          setUpdatedTaskDeadline(selectedTask.taskDeadline);
+          setUpdatedTaskStatus(selectedTask.taskStatus); 
+          setUpdatedTaskOwner(selectedTask.taskOwner);
+      }
+      setShowUpdatePopup(true);
+      setShowMenu(false);
+  };
+
+    const handleDeleteClick = () => {
+        setShowDeletePopup(true);
+        setShowMenu(false);
+    };
+
+    const updateTask = async () => {
+      try {
+          const updatedTaskInfo = {
+              taskName: updatedTaskName,
+              taskDeadline: updatedTaskDeadline,
+              taskStatus: updatedTaskStatus,
+              taskOwner: updatedTaskOwner
+          };
+          await axios.put(`http://localhost:3000/hh/${selectedTask._id}`, updatedTaskInfo);
+          fetchTasks();
+          setShowUpdatePopup(false);
+      } catch (error) {
+          console.error("Error updating task:", error);
+      }
+  };
+
+    const deleteTask = async () => {
+        try {
+            await axios.delete(`http://localhost:3000/hh/${selectedTask._id}`);
+            fetchTasks();
+            setShowDeletePopup(false);
+        } catch (error) {
+            console.error("Error deleting task:", error);
+        }
+    };
+
+    const renderTaskSquare = (task) => {
+        return (
+            <div key={task._id} className="task-square">
+                <h3>{task.taskName}</h3>
+                <p>Deadline: {new Date(task.taskDeadline).toLocaleDateString()}</p>
+                <p>Owner: {task.taskOwner}</p>
+                <button className="menu-button" onClick={() => handleMenuClick(task)}>&#x22EE;</button>
+                {showMenu && selectedTask._id === task._id && (
+                    <div className="task-menu">
+                        <button onClick={handleUpdateClick}>Update</button>
+                        <button onClick={handleDeleteClick}>Delete</button>
+                    </div>
+                )}
+            </div>
+        );
+    };
+
+    return (
+        <div className="task-board">
+            {["To Do", "In Progress", "Done"].map((status) => (
+                <div key={status} className="column">
+                    <h2>{status}</h2>
+                    {tasks.filter(task => task.taskStatus === status).map(renderTaskSquare)}
+                </div>
+            ))}
+
+        {showUpdatePopup && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <h3>Update Task</h3>
+                        {selectedTask && (
+                            <>
+                                <input 
+                                    type="text" 
+                                    value={updatedTaskName} 
+                                    onChange={(e) => setUpdatedTaskName(e.target.value)} 
+                                />
+                                <input 
+                                    type="date" 
+                                    value={updatedTaskDeadline} 
+                                    onChange={(e) => setUpdatedTaskDeadline(e.target.value)} 
+                                />
+                                <select 
+                                    value={updatedTaskStatus} 
+                                    onChange={(e) => setUpdatedTaskStatus(e.target.value)}>
+                                    <option value="To Do">To Do</option>
+                                    <option value="In Progress">In Progress</option>
+                                    <option value="Done">Done</option>
+                                </select>
+                                <input 
+                                    type="text" 
+                                    value={updatedTaskOwner} 
+                                    onChange={(e) => setUpdatedTaskOwner(e.target.value)} 
+                                />
+                            </>
+                        )}
+                        <button onClick={updateTask}>Save</button>
+                        <button onClick={() => setShowUpdatePopup(false)}>Cancel</button>
+                    </div>
+                </div>
+            )}
+
+            {showDeletePopup && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <h3>Are you sure you want to delete this task?</h3>
+                        <button onClick={deleteTask}>Confirm</button>
+                        <button onClick={() => setShowDeletePopup(false)}>Cancel</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
-return (
-  <div className="task-board">
-      <div className="column">
-          <h2>To Do</h2>
-          {filterTasksByStatus("To Do").map(task => (
-              <div key={task._id} className="task-square">
-                  <h3>{task.taskName}</h3>
-                  <p>Deadline: {new Date(task.taskDeadline).toLocaleDateString()}</p>
-                  <p>Owner: {task.taskOwner}</p>
-              </div>
-          ))}
-      </div>
-      <div className="column">
-          <h2>In Progress</h2>
-          {filterTasksByStatus("In Progress").map(task => (
-              <div key={task._id} className="task-square">
-                  <h3>{task.taskName}</h3>
-                  <p>Deadline: {new Date(task.taskDeadline).toLocaleDateString()}</p>
-                  <p>Owner: {task.taskOwner}</p>
-              </div>
-          ))}
-      </div>
-      <div className="column">
-          <h2>Done</h2>
-          {filterTasksByStatus("Done").map(task => (
-              <div key={task._id} className="task-square">
-                  <h3>{task.taskName}</h3>
-                  <p>Deadline: {new Date(task.taskDeadline).toLocaleDateString()}</p>
-                  <p>Owner: {task.taskOwner}</p>
-              </div>
-          ))}
-      </div>
-  </div>
-);
-}
-
 
 export default TaskBoard;
