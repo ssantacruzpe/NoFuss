269884aa2dd25aff4c5bdb615adaf14498d3546a
ssantacruzpe <stephanysantacruz@protonmail.com>
2023-11-22 23:44:07 +0100

    NavBar x Boostrap first try

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "./Components/SignUpPage";
 import LoginPage from "./Components/LoginPage";
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./NavBar.css"; 
 
 const NavBar = () => {
     return (
         <div className="navbar">
-            <div className="logo">NoFuss</div>
-            <nav>
-                <ul>
-                    <li><a href="http://localhost:3001/welcome">Home</a></li>
-                    <li><a href="http://localhost:3001/hh">House Hurdles</a></li>
-                </ul>
-            </nav>
+        <div className="sidebar d-flex flex-column flex-shrink-0 p-3" style={{width: "280px"}}>
+            <a href = "/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <span className="logo">NoFuss</span>
+            </a>
+            <hr/>
+            <ul className="nav nav-pills flex-column">
+                <li className="nav-item">
+                    <a href="http://localhost:3001/welcome" className = "nav-link active" aria-current = "page">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="http://localhost:3001/hh" className="nav-link link-dark">
+                        House Hurdles
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="nav-link link-dark">
+                        LogOut
+                    </a>
+                </li>
+            </ul>
+            <hr/>
+        </div>
         </div>
     );
 }
