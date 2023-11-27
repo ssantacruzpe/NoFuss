e25013fef389f353c0481fc77ad598e339af7b69
ssantacruzpe <stephanysantacruz@protonmail.com>
2023-11-27 22:53:53 +0100

    tweak styling task board

--- client/src/Components/FilterOwner.js
+++ client/src/Components/FilterOwner.js
@@ -5,13 +5,15 @@ import { faFilter } from '@fortawesome/free-solid-svg-icons';
 function FilterOwner({ owners, onOwnerSelect }) {
     return (
         <div className="filter-owner">
-            <FontAwesomeIcon icon={faFilter} className="filter-icon" />
-            <select onChange={(e) => onOwnerSelect(e.target.value)} className="owner-select">
-                <option value="">All Owners</option>
-                {owners.map((owner, index) => (
-                    <option key={index} value={owner}>{owner}</option>
-                ))}
-            </select>
+            <div className="owner-box">
+                <FontAwesomeIcon icon={faFilter} className="filter-icon" />
+                <select className="owner-select" onChange={(e) => onOwnerSelect(e.target.value)} >
+                    <option value="">All Owners</option>
+                    {owners.map((owner, index) => (
+                        <option key={index} value={owner}>{owner}</option>
+                    ))}
+                </select>
+            </div>
         </div>
     );
 }
